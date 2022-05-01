e44a7d9ce6feac393d19642533efa495c3689eb6
afrisanjaya <47937342+afrisanjaya@users.noreply.github.com>
2022-05-02 01:46:05 +0700

    Change variable image view

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var diceImageOne: UIImageView!
+    
+    
+    @IBOutlet weak var diceImageTwo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageOne.image = UIImage(named: "DiceSix")
+        diceImageOne.alpha = 1.5
+        
+        diceImageTwo.image = UIImage(named: "DiceFive")
+        diceImageTwo.alpha = 0.5
     }
 
 
